e9612ecaacfa605a5ec29cb38626384b70e86641
Diefex <diefex57@gmail.com>
2022-12-18 18:12:32 -0500

    ajustes en la responsividad

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout'
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-principal',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent {
-  showFiller = false;
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  toolbar!: MatToolbar;
+
+  small = false;
+
+  constructor(private observer: BreakpointObserver) { }
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 600px)']).subscribe((res) => {
+      if (res.matches) {
+        this.small = true;
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.small = false;
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
 }

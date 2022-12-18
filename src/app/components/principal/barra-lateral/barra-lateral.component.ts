414104e380c6a49f14e7bb52d43fed1132934ed1
jordy pineda velandia <jepinedav@gmail.com>
2022-12-18 15:41:12 -0500

    servise eventos 0.1

--- src/app/components/principal/barra-lateral/barra-lateral.component.ts
+++ src/app/components/principal/barra-lateral/barra-lateral.component.ts
@@ -1,6 +1,6 @@
 import { style } from '@angular/animations';
 import { Component,ElementRef,ViewChild, Renderer2 } from '@angular/core';
-let ancho:number=0;
+
 
 
 @Component({
@@ -11,21 +11,6 @@ let ancho:number=0;
 export class BarraLateralComponent {
 
   constructor(private render2: Renderer2){
-
   }
   
- @ViewChild('nav_lat')nav!: ElementRef;
-
-  menuDesplegar():void{
-
-    if(ancho==0){
-      ancho=80;
-    }else{
-      ancho=0;
-    }
-    let nav_des= this.nav.nativeElement;
-    this.render2.setStyle(nav_des,'width', ancho+'%');
-  }
-
-
 }
